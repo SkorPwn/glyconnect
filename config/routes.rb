d95619d6e4d2ec34326c82efa7314587b92086ec
SkorPwn <kevin.jj.tran@gmail.com>
2016-01-23 22:07:23 +0100

    loginlogoff

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :patients
   get 'pages/accueil'
 
   get 'pages/patients'
 
   get 'accueil/Patients'
-  root 'patients#index'
+
+  root 'pages#accueil'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
